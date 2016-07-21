f436dda1023f23294a1fb2096121d527c7b36f96
Noureldin <noureldinyosri@gmail.com>
2016-07-21 16:51:20 +0200

    last update

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,5 +2,47 @@
  * Created by noureldin on 7/21/16.
  */
 public class Game {
+    private int SIZE;
+    private int [] [] State;
+    public Game(){
+        SIZE = 3;
+        State = new int[SIZE][SIZE];
+    }
+    public Game(int size){
+        SIZE = size;
+        State = new int[SIZE][SIZE];
+    }
+    public int getSize(){
+        return SIZE;
+    }
+    public int [] [] getState(){
+        int [] [] ret = new int[SIZE][SIZE];
+        for(int i = 0;i < SIZE;i++) for(int j = 0;j < SIZE;j++) ret[i][j] = State[i][j];
+        return ret;
+    }
+    public static Boolean isValidState(int [] [] State){
+        if(State == null) return false;
+        int ctr1 = 0,ctr2 = 0,n = State.length;
+        for(int i = 0;i < n;i++) if(State[i].length != n) return false;
+        for(int i = 0;i < n;i++) for(int j = 0;j < n;j++) {
+            if(State[i][j] == 1) ctr1++;
+            if(State[i][j] == 2) ctr2++;
+            if(State[i][j] < 0 || State[i][j] > 2) return false;
+        }
+        if(Math.abs(ctr1 - ctr2) > 1) return false;
+        return true;
+    }
 
+    public boolean makeMove(int player,int x,int y){
+        if(x < 0 || x >= SIZE || y < 0 || y >= SIZE || (player != 1 && player != 2)) return false;
+        if(State[x][y] != 0) return false;
+        State[x][y] = player;
+        return isValidState(State);
+    }
+
+/*
+    public static int getGameState(int [] [] State){
+
+    }
+*/
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,16 +1,72 @@
-import ...;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
 
 /**
  * Created by noureldin on 7/21/16.
  */
+@RunWith(JUnitParamsRunner.class)
 public class GameTest {
     @Test
-    public void testDefaultConstructor() throws Exception{
-        final Game G = new Game();
-        assertEquals("Default Size is ",G.getSize(),3);
-        assertEquals()
+    public void testDefaultConstructor() throws Exception {
+        Game G = new Game();
+        int [] [] expectedState = new int[3][3];
+        assertEquals("Default Game Size",G.getSize(),3);
+        assertEquals("Default Game State",G.getState(),expectedState);
     }
 
-    public void testConsturctor
+    @Test
+    @Parameters (
+            {
+                "5",
+                "6"
+            }
+    )
+    public void testConstructor(int a) throws Exception {
+        Game G = new Game(a);
+        assertEquals("Constructor retSize ",a,G.getSize());
+        assertEquals("Constructor State",new int [a][a],G.getState());
+    }
+
+
+    @Test
+     public void testIsValidState1(){
+        int [][] State = {{1,1,1},{0,0,0},{0,0,0}};
+        Boolean correctState = false;
+        assertEquals("test validity",correctState,Game.isValidState(State));
+    }
+    @Test
+    public void testIsValidState2(){
+        int [][] State = {{1,2,1},{0,0,0},{0,0,0}};
+        Boolean correctState = true;
+        assertEquals("test validity",correctState,Game.isValidState(State));
+    }
+    @Test
+    public void testMakeMoveOutOfBound(){
+        Game G = new Game();
+        assertEquals("test MakeMove",false,G.makeMove(1,-1,0));
+    }
+    @Test
+    public void testMakeMoveImaginaryPlayer(){
+        Game G = new Game();
+        assertEquals("test MakeMove",false,G.makeMove(3,0,0));
+    }
+    @Test
+    public void testMakeMoveCorrectPlayer(){
+        Game G = new Game();
+        assertEquals("test MakeMove",true,G.makeMove(1,0,0));
+    }
+    
+
+    /*
+    protected void getGameState(int [] [] State,int correctState) throws Exception {
+        assertEquals("Game State",correctState,Game.getGameState(State));
+    }*/
 
 }
